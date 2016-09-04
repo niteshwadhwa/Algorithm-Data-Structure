4665bc1e66626d22556c234da238e0bff14435e5
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-09-04 12:54:02 -0700

    Remove Element

---
+++ RemoveElement.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int length = nums.length;
+        int count  =  0;
+        
+        for(int i=0;i<length;i++){
+            if(nums[i] == val){
+                nums[i] = nums[length - 1];
+                i--;
+                length--;
+            }else{
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
