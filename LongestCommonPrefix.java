d1d5962c41f03696324fa1e9fdbbf6dcd1a9ce33
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-10-01 22:49:23 -0700

    Lowest common prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if(strs.length == 0){
+            return "";
+        }else{
+            
+            String pre = strs[0];
+            for(int i=1;i<strs.length;i++){
+                while(strs[i].indexOf(pre) != 0){
+                    pre = pre.substring(0,pre.length()-1);
+                }
+            }
+            return pre;
+        }
+        
+    }
+}
