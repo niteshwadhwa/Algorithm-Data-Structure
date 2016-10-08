8fc926efd79f9dddd6f065141fb16903f1db0a2d
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-10-08 14:06:57 -0700

    Integer to Hexa Decimal Number

---
+++ ConvertNumberToHexaDecimal.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String toHex(int num) {
+        
+        char hex[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        
+        if(num == 0){
+            return "0";
+        }
+        String result = "";
+        while(num != 0){
+            result = hex[(num & 15)] + result;
+            num = num >>> 4;
+        }
+        
+        return result;
+        
+    }
+}
