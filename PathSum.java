91a267f1645fbdb189c40378d467f359101e779d
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-11-05 17:46:27 -0700

    PathSum

--- PathSum.java
+++ PathSum.java
@@ -9,40 +9,21 @@
  */
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-         
-         if(root == null){
-             return false;
-         }if(root.left == null && root.right == null && root.val == sum){
-             return true;
-         }else{
-             return calculateSum(root, root.val, sum);
-         }
-         
-    }
-    
-    public boolean calculateSum(TreeNode root, int count, int sum){
-      
-       if(root.left != null){
-           boolean value = calculateSum(root.left,count+root.left.val, sum);
-           if(value == true){
-               return true;
-           }
-       }
-       
-       if(root.right != null){
-           boolean value = calculateSum(root.right,count+root.right.val, sum);
-           if(value == true){
-              return true;
-           }
-       }
-       
-       if(root.left == null && root.right == null && count == sum){
-           return true;
-       }else{
-           return false;
-       }
+        
+        if(root == null){
+            return false;
+        }
+        
+        if(hasPathSum(root.left, sum-root.val)) return true;
+        if(hasPathSum(root.right, sum-root.val)) return true;
+        
+        if(root.left == null && root.right == null && sum - root.val == 0){
+            return true;
+        }else{
+            return false;
+        }
         
     }
-    
+
     
 }
