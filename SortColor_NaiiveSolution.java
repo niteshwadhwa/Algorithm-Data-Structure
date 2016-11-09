669951684d4bb6218db7bc7d935c61b186a48b9c
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-11-08 18:40:22 -0800

    Sort Color

--- SortColor_NaiiveSolution.java
+++ SortColor_NaiiveSolution.java
@@ -1,50 +1,55 @@
 public class Solution {
     public void sortColors(int[] nums) {
-        
-        TreeSet<Integer> arr = new TreeSet<Integer>();
-        int start = 0, end = nums.length-1;
+      
+        int start = 0;
+        int end = nums.length-1;
         
         while(start <= end){
             
-            if(nums[start] == 0 && arr.size() == 0){
-                 start++;
-            }else if(nums[start] == 0 && arr.size() > 0){
-                int temp1= arr.pollFirst();
-                nums[temp1] = 0;
-                nums[start] = 1;
-                arr.add(start);
+            while(start <= end && nums[start] == 0){
                 start++;
-            }else if(nums[start] == 1){
-                arr.add(start);
-                start++;
-            }else if(nums[start] == 2 && nums[end] != 2){
-                
-                int temp = nums[end];
-                nums[end] = nums[start];
-                nums[start] = temp;
-                
-                if(nums[start] != 0){
-                    arr.add(start);
-                    start++;
-                }else if(nums[start] == 0 && arr.size() > 0){
-                    int temp1= arr.pollFirst();
-                    nums[temp1] = 0;
-                    nums[start] = 1;
-                    arr.add(start);
-                    start++;
-                }else{
-                    start++;
-                }
-                
-            }else{
+            }
+            
+            while(start <= end && nums[end] != 0){
                 end--;
             }
             
+            if(start < end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }else{
+                break;
+            }
+            
         }
-        
+       
+        start = 0;
+        end = nums.length-1;
+        while(start <= end){
+            
+            while(start <= end && (nums[start] == 0 || nums[start] == 1)){
+                start++;
+            }
+            
+            while(start <= end && nums[end] == 2){
+                end--;
+            }
+            
+            if(start < end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }else{
+                break;
+            }
+            
+        }
+       
         
     }
-    
-    
-    
 }
