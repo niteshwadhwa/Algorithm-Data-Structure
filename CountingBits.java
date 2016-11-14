a68792b60b9a87d0d0a61ac32d7d3165a33beb5d
niteshwadhwa <nitesh.wadhwa1099@gmail.com>
2016-11-14 12:46:43 -0800

    CountingBits Update

--- CountingBits.java
+++ CountingBits.java
@@ -1,10 +1,22 @@
 public class Solution {
     public int[] countBits(int num) {
+        
         int[] arr = new int[num+1];
-        arr[0] = 0;
+        int n = 0;
+
         for(int i=0;i<=num;i++){
-           arr[i] = Integer.bitCount(i);
+            
+            n = i;
+            while(n != 0){
+                if((n&1) == 1){
+                    arr[i] = arr[i]+1;
+                }
+                n = n>>1;
+            }
+                
         }
+        
         return arr;
+        
     }
 }
